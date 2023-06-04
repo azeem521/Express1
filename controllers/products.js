92a90be802683d27d94b0ffa504a1cd840e3f58f
azeem521 <azeemsre1@gmail.com>
2023-06-04 14:42:27 +0530

    modal added and save temporary data (not used fileSystem)

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,7 @@
 
-const products=[];
+// const products=[];
+
+const Product =require('../modals/product')
 
 exports.getAddProduct= (req, res, next) => {
     res.render('add-product', {
@@ -12,12 +14,14 @@ exports.getAddProduct= (req, res, next) => {
   }
 
   exports.postAddProduct=(req, res, next) => {
-    products.push({ title: req.body.title });
+    // products.push({ title: req.body.title });
+    const product=new Product(req.body.title);
+    product.save();
     res.redirect('/');
   }
 
   exports.getProducts =(req, res, next) => {
- 
+ const products =Product.fetchAll();
     res.render('shop', {
       prods: products,
       pageTitle: 'Shop',
