9ad9b4f50e20abea804d83c4f07f0d0987b57d47
azeem521 <azeemsre1@gmail.com>
2023-06-04 12:53:49 +0530

    controller added

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+// const adminData = require('./routes/admin');
+
+const adminRoute = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoute);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
